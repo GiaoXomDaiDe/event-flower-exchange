3bffc4f3845299ce94561cefa771effffe752230
NGUYEN_HA_LAM <lamnhse170579@fpt.edu.vn>
2024-10-03 22:35:50 +0700

    "Complete Sign-in and Sign-up"

--- EventFlowerExchange_Espoir/EventFlowerExchange_Espoir/Services/IAccountService.cs
+++ EventFlowerExchange_Espoir/EventFlowerExchange_Espoir/Services/IAccountService.cs
@@ -1,6 +1,17 @@
-﻿namespace EventFlowerExchange_Espoir.Services
+﻿using EventFlowerExchange_Espoir.Models;
+using EventFlowerExchange_Espoir.Models.DTO;
+
+namespace EventFlowerExchange_Espoir.Services
 {
     public interface IAccountService
     {
+        public Task<Account> GetAccountByEmailAsync(string email);
+        public Task<Account> GetAccountByPhoneAsync(string phone);
+
+        public Task<dynamic> RegisterAccountAsync(AccountDTO accountDTO);
+        public Task<Account> CheckLogin(string email, string password);
+        public Task<AuthResponseDTO> GetFirebaseToken(string firebaseToken);
+        public string GenerateJwtToken(string email, int Role, double expirationMinutes);
+        public Task<dynamic> SignUpByGoogleAsync(string firebaseToken, string phone, DateOnly birthday, string address, int gender);
     }
 }
